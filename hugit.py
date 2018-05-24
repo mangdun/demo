ecb891928689462dfb46bef658ac8bde35cd35e2
xwp <xwp_fullstack@163.com>
2018-05-24 09:01:59 +0800

    修订版1

--- hugit.py
+++ hugit.py
@@ -10,11 +10,7 @@ def login():
 def register():
 	return 'register'
 
-<<<<<<< HEAD
 @route('/new')
-=======
-@route('/')
->>>>>>> be23baf0aa16ae2ef27b4e8613999e8e626b82ad
 def new():
 	return 'new'
 
